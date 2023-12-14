1a4183f0f295db6e22ac824369fef17734cee4ca
Gourav Bhati <gouravb791@gmail.com>
2023-12-15 00:55:07 +0530

    changes un the env file

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,55 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
-import axios from "axios";
-// import TypingAnimation from "../components/TypingAnimation";
-import TypingAnimation from "./components/TypingAnimation";
 import "./App.css";
 
 export default function App() {
   const [inputValue, setInputValue] = useState("");
   const [chatLog, setChatLog] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  // Fetching
+  const fetchFromAPI = async (prompt) => {
+    const url = "https://open-ai21.p.rapidapi.com/conversationgpt";
 
+    const options = {
+      method: "POST",
+
+      headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "a0bd01d45amshcf8ae2d6e7a5e9bp1c10dajsn1d2877b423a3",
+        "X-RapidAPI-Host": "open-ai21.p.rapidapi.com",
+      },
+      body: JSON.stringify({
+        messages: [
+          {
+            role: "user",
+            content: prompt,
+          },
+        ],
+        web_access: false,
+        stream: false,
+      }),
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      setChatLog((prevChatLog) => [
+        ...prevChatLog,
+        { type: "bot", message: result.GPT },
+      ]);
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSubmit = () => {
     setChatLog((prevChatLog) => [
       ...prevChatLog,
       { type: "user", message: inputValue },
     ]);
-
-    sendMessage(inputValue);
-
-    setInputValue("");
-  };
-
-  const sendMessage = (message) => {
-    const url = "/api/chat";
-
-    const data = {
-      model: "gpt-3.5-turbo-0301",
-      messages: [{ role: "user", content: message }],
-    };
-
-    setIsLoading(true);
-
-    axios
-      .post(url, data)
-      .then((response) => {
-        // console.log(response);
-        setChatLog((prevChatLog) => [
-          ...prevChatLog,
-          { type: "bot", message: response.data.choices[0].message.content },
-        ]);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setIsLoading(false);
-        console.log(error);
-      });
+    fetchFromAPI(inputValue);
+    setInputValue(" ");
   };
 
   return (
@@ -72,32 +76,32 @@ export default function App() {
                 </div>
               </div>
             ))}
-            {isLoading && (
+            {/* {isLoading && (
               <div key={chatLog.length} className="flex justify-start">
                 <div className="bg-gray-800 rounded-lg p-4 text-white max-w-sm">
                   <TypingAnimation />
                 </div>
               </div>
-            )}
+            )} */}
           </div>
         </div>
-        <form onSubmit={handleSubmit} className="flex-none p-6">
-          <div className="flex rounded-lg border border-gray-700 bg-gray-800">
-            <input
-              type="text"
-              className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none"
-              placeholder="Type your message..."
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-            />
-            <button
-              type="submit"
-              className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300 "
-            >
-              Send
-            </button>
-          </div>
-        </form>
+        {/* <form onSubmit={handleSubmit} className="flex-none p-6"> */}
+        <div className="flex rounded-lg border border-gray-700 bg-gray-800">
+          <input
+            type="text"
+            className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none"
+            placeholder="Type your message..."
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button
+            onClick={handleSubmit}
+            className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300 "
+          >
+            Send
+          </button>
+        </div>
+        {/* </form> */}
       </div>
     </div>
   );
